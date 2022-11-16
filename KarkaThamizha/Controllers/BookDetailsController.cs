bb3d4cbca6cb8a39742700a40d6a2eeb69f1d3cf
Sahul Hameed <sahulhameediqbal@gmail.com>
2022-11-16 21:40:55 +0530

    16thJuly2022

---
+++ KarkaThamizha/Controllers/BookDetailsController.cs
@@ -0,0 +1,26 @@
+﻿using KarkaThamizha.Object.Models;
+using KarkaThamizha.Repository.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KarkaThamizha.Controllers
+{
+    public class BookDetailsController : Controller
+    {
+        public ActionResult BookDetails(int? id)
+        {
+            BooksDetailsModels mdlBookDetails = null;
+            BooksDetailsRepository repoBookDetails = null;
+            if (id > 0)
+            {
+                //mdlBookDetails = new BooksDetailsModels();
+                //repoBookDetails = new BooksDetailsRepository();
+                //mdlBookDetails = repoBookDetails.GetBooksDetailsByBookID(id.Value);
+            }
+            return View("BookDetails", mdlBookDetails);
+        }
+    }
+}
